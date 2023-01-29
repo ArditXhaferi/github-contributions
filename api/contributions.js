a6d3eed937ed93e1e6c4302d9a5802018e1a0529
ArditXhaferi <ardit@starlabspro.com>
2023-01-29 21:43:29 +0100

    Small change lol

--- api/contributions.js
+++ api/contributions.js
@@ -8,10 +8,39 @@ const router = express.Router();
  */
 router.get("/", async (req, res) => {
   try {
-    res.json({
-      status: 200,
-      message: "Testing if this works",
-    });
+    const githubRequest = (url, name) => {
+      return new Promise((resolve, reject) => {
+          fetch(url.replace("$name$", name))
+              .then((response) => response.json())
+              .then((data) => {
+                  let MyDate = new Date();
+                  let MyDateString;
+  
+                  MyDateString = MyDate.getFullYear() + '-' + ('0' + (MyDate.getMonth() + 1)).slice(-2) + "-" + ('0' + MyDate.getDate()).slice(-2);
+  
+                  data.contributions.forEach((contribution, index) => {
+                      if (contribution.date == MyDateString) {
+                          resolve(data.contributions.slice(index, index + 30))
+                      }
+                  });
+              })
+              .catch((error) => {
+                  reject(error);
+              });
+      });
+  };
+
+  try {
+      let contributions = await githubRequest(
+          `https://github-contributions.vercel.app/api/v1/$name$`,
+          req.query.username
+      );
+
+      res.json(contributions);
+  } catch (error) {
+      console.error(error);
+      return res.status(500).send("Server error");
+  }
   } catch (error) {
     console.error(error);
     return res.status(500).send("Server error");
